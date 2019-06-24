2a1233ebbb1cddb636e6650c13d1386a4267bfe4
shyboylpf <lpf.mart@gmail.com>
2019-06-25 07:35:41 +0800

    匿名函数

--- ThreadTest7/Program.cs
+++ ThreadTest7/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+// Creating and Starting Threads
+using System;
+using System.Threading;
 
 namespace ThreadTest7
 {
@@ -6,7 +9,24 @@ namespace ThreadTest7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 用ThreadStart
+            //Thread t = new Thread(new ThreadStart(Go));
+
+            // A thread can be created more conveniently by specifying just a method group — and allowing C# to infer the ThreadStart delegate:
+            // ThreadStart 可以省略
+            //Thread t = new Thread(Go);
+
+            // Another shortcut is to use a lambda expression or anonymous method:
+            // 另一种缩写方式, 使用lambda表达式或者匿名函数
+            Thread t = new Thread(()=> { Console.WriteLine("Hello!"); });
+
+            t.Start();
+            Go();
+        }
+
+        static void Go()
+        {
+            Console.WriteLine("hello");
         }
     }
 }
