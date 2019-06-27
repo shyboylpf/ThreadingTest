3a6d82877f4a9bc6b339185a2754d1a066d56e8e
shyboylpf <lpf.mart@gmail.com>
2019-06-28 07:38:48 +0800

    其他线程抛出的异常, 并不会被主线程catch到

---
+++ ThreadTest13/Program.cs
@@ -0,0 +1,29 @@
+﻿///Exception Handling
+///Any try/catch/finally blocks in scope when a thread is created are of no relevance to the thread when it starts executing.
+///Consider the following program:
+
+using System;
+using System.Threading;
+
+namespace ThreadTest13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///The try/catch statement in this example is ineffective, and the newly created thread will be encumbered with an unhandled 
+            ///NullReferenceException. This behavior makes sense when you consider that each thread has an independent execution path.
+            try
+            {
+                new Thread(Go).Start();
+            }
+            catch (Exception ex)
+            {
+                // We'll never get here!
+                Console.WriteLine("Exception!");
+            }
+        }
+
+        static void Go() { throw null; }   // Throws a NullReferenceException
+    }
+}
