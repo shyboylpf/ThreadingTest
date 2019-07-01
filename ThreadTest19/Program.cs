45db6187e9a9b88eecfee506f60e0dab7dcfbb9c
shyboylpf <lpf.mart@gmail.com>
2019-07-01 10:49:34 +0800

    异步委托, BeginInvoke Func来实现线程的双向传值

---
+++ ThreadTest19/Program.cs
@@ -0,0 +1,49 @@
+﻿///Asynchronous delegates
+///ThreadPool.QueueUserWorkItem doesn’t provide an easy mechanism for getting return values back from a thread after it has finished executing.Asynchronous
+///delegate invocations (asynchronous delegates for short) solve this, allowing any number of typed arguments to be passed in both directions. Furthermore, 
+///unhandled exceptions on asynchronous delegates are conveniently rethrown on the original thread (or more accurately, the thread that calls EndInvoke), 
+///and so they don’t need explicit handling.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest19
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Here’s how you start a worker task via an asynchronous delegate:
+
+        //  Instantiate a delegate targeting the method you want to run in parallel (typically one of the predefined Func delegates).
+        //Call BeginInvoke on the delegate, saving its IAsyncResult return value.
+
+        //BeginInvoke returns immediately to the caller. You can then perform other activities while the pooled thread is working.
+        //When you need the results, call EndInvoke on the delegate, passing in the saved IAsyncResult object.
+        //In the following example, we use an asynchronous delegate invocation to execute concurrently with the main thread, a simple method that returns a string’s length:
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Func<string, int> method = Work;
+            //IAsyncResult cookie = method.BeginInvoke("test", null, null);
+            ///// 
+            ///// ... here's where we can do other work in parallel...
+            /////
+            //int result = method.EndInvoke(cookie);
+
+
+            /// 由于.netCore BeginInvoke不支持 , 那我们就用Task来写吧. ThreadTest20有.netFramework版本的BeginInvoke.
+            Task<int> task = new Task<int>(() => Work("test"));
+            task.Start();
+            task.Wait();
+            Console.WriteLine("String length is : " + task.Result);
+        }
+
+        static int Work(string s)
+        {
+            return s.Length;
+        }
+    }
+}
---
+++ ThreadTest20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThreadTest20
+{
+    class Program
+    {
+        /// <summary>
+        /// 书接上回, ThreadTest19.netCore竟然不支持这样的操作
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Func<string, int> method = Work;
+            IAsyncResult cookie = method.BeginInvoke("test", null, null);
+            int result = method.EndInvoke(cookie);
+            Console.WriteLine("String length is : " + result);
+        }
+        static int Work(string s)
+        {
+            return s.Length;
+        }
+    }
+}
