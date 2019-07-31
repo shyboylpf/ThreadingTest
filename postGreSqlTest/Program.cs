a4b611d5318deff1523dc3f231c60a14679b5a99
shyboylpf <lpf.mart@gmail.com>
2019-07-31 10:35:07 +0800

    postgre测试

---
+++ postGreSqlTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Light.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postGreSqlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContextConfiguration.SetConfigFilePath("Config/lightdata.json");
+            var context = new DataContext();
+            //var item = context.Query<mattressdatacollectlist>().Where(x => x.collecttype == 1).First();
+            var sql = "SELECT * FROM mattressdatacollectlist;";
+            var executor = context.CreateSqlStringExecutor(sql);
+            var ret = executor.ExecuteNonQuery();
+        }
+    }
+}
---
+++ postGreSqlTest/mattressdatacollectlist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace postGreSqlTest
+{
+    public class mattressdatacollectlist
+    {
+        public DateTime collectdt;
+        public int collecttype;
+        public int mattressid;
+        public int sickbedid;
+        public int samplesourceid;
+        public string devicecollectval;
+
+    }
+}
