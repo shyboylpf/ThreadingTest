9944fe16a3ef25d0ff5708ed8c85ec2e326b1a14
shyboylpf <lpf.mart@gmail.com>
2019-08-16 16:57:11 +0800

    需要加锁的时机

---
+++ ThreadTest27/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// When to Lock
+/// As a basic rule, you need to lock around accessing any writable shared field. Even in the simplest case — an assignment operation on a single field — you must consider synchronization. In the following class, neither the Increment nor the Assign method is thread-safe:
+/// </summary>
+namespace ThreadTest27
+{
+    class Program
+    {
+        static int _x;
+        static void Main(string[] args)
+        {
+        }
+
+        // 这些都是线程不安全的
+        static void Increment() { _x++; }
+        static void Assign() { _x = 123; }
+
+        // 以下是Thread-safe版本
+        static readonly object _locker = new object();
+        static int _x1;
+        static void Increment1()
+        {
+            lock (_locker)
+            {
+                _x1++;
+            }
+        }
+        static void Assign1()
+        {
+            lock (_locker)
+            {
+                _x1 = 123;
+            }
+        }
+
+    }
+}
