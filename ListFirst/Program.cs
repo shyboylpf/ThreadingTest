c1d8034363b16f4ea7733987cbe1555261398c76
shyboylpf <lpf.mart@gmail.com>
2019-08-16 14:58:07 +0800

    list的first函数取不出值会throw Exception

---
+++ ListFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<tmp> tmps = new List<tmp>();
+            Console.WriteLine(tmps.First(x=>x.sn.Equals("1")));
+        }
+    }
+
+    class tmp
+    {
+        public string sn { get; set; }
+    }
+}
