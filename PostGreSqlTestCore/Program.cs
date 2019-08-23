91783b1b47ea944ef69be3a317042604d3e5da9c
shyboylpf <lpf.mart@gmail.com>
2019-08-23 17:21:37 +0800

    postGreCoreTest

---
+++ PostGreSqlTestCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PostGreSqlTestCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
