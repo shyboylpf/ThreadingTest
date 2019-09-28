18b6bf88f54d2fede1bf7fb596e14fe75d82b005
shyboylpf <lpf.mart@gmail.com>
2019-09-29 00:48:08 +0800

    Cross-Process EventWaitHandle 跨进程的

---
+++ TreadTest51/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TreadTest51
+{
+    /// <summary>
+    /// Creating a Cross-Process EventWaitHandle
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            ///EventWaitHandle’s constructor allows a “named” EventWaitHandle to be created, capable of operating
+            ///across multiple processes. The name is simply a string, and it can be any value that doesn’t unintentionally
+            ///conflict with someone else’s! If the name is already in use on the computer, you get a reference to the same
+            ///underlying EventWaitHandle; otherwise, the operating system creates a new one. Here’s an example:
+            EventWaitHandle wh = new EventWaitHandle(false,EventResetMode.AutoReset, "MyCompany.MyApp.SomeName");
+        }
+    }
+}
