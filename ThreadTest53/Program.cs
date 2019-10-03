84ffd69da3d19aad5134710e4549a505e227ff63
shyboylpf <lpf.mart@gmail.com>
2019-10-03 16:57:37 +0800

    Barrier 可以等着几个signal一起触发run

---
+++ ThreadTest53/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadTest53
+{
+    class Program
+    {
+        static Barrier _barrier = new Barrier(3, barrier => Console.WriteLine());
+        static void Main(string[] args)
+        {
+            new Thread(Speak).Start();
+            new Thread(Speak).Start();
+            new Thread(Speak).Start();
+        }
+
+        private static void Speak()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(i + " ");
+                _barrier.SignalAndWait();
+            }
+        }
+    }
+}
