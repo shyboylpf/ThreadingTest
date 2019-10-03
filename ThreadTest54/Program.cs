09d2678fc3b151aa6faefc73603fcfadeb973285
shyboylpf <lpf.mart@gmail.com>
2019-10-03 22:11:43 +0800

    Synchronization Contexts , CLR确保一次只有一个线程可以在safeInstance中执行代码。

---
+++ ThreadTest54/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+
+namespace ThreadTest54
+{
+
+    [Synchronization]
+    public class AutoLock : ContextBoundObject
+    {
+        public void Demo()
+        {
+            Console.Write("Start...");
+            Thread.Sleep(1000);           // We can't be preempted here
+            Console.WriteLine("end");     // thanks to automatic locking!
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoLock safeInstance = new AutoLock();
+            new Thread(safeInstance.Demo).Start();      // call the demo method
+            new Thread(safeInstance.Demo).Start();      // three times
+            safeInstance.Demo();                        // concurrently.
+        }
+    }
+}
