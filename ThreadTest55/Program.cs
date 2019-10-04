87717d33474d73adeb8eac556d7531cf32b24f7f
shyboylpf <lpf.mart@gmail.com>
2019-10-04 20:57:34 +0800

    Synchronization Contexts 锁定以相同的方式在内部应用。

---
+++ ThreadTest55/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+
+namespace ThreadTest55
+{
+    [Synchronization]
+    public class AutoLock : ContextBoundObject
+    {
+        public void Demo()
+        {
+            Console.Write("Start...");
+            Thread.Sleep(1000);
+            Console.WriteLine("end");
+        }
+
+        public void Test()
+        {
+            new Thread(Demo).Start();
+            new Thread(Demo).Start();
+            new Thread(Demo).Start();
+            Console.ReadLine();
+        }
+
+        public static void Main()
+        {
+            new AutoLock().Test();
+        }
+    }
+
+    [Synchronization(SynchronizationAttribute.REQUIRES_NEW)]
+    public class SynchronizedB : ContextBoundObject
+    {
+
+    }
+}
