f48ea0c510b32e353bd864ed6d810e85c666e585
shyboylpf <lpf.mart@gmail.com>
2019-10-05 14:50:16 +0800

    BackgroundWorker

---
+++ ThreadTest58/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
+
+namespace ThreadTest58
+{
+    class Program
+    {
+        static BackgroundWorker _bw = new BackgroundWorker();
+        static void Main(string[] args)
+        {
+            _bw.DoWork += bw_DoWork;
+            _bw.RunWorkerAsync("Message to worker.");
+            Console.ReadLine();
+        }
+
+        private static void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // THis is called on the worker thread
+            Console.WriteLine(e.Argument);      // Writes "Message to worker."
+            // Perform time-consuming task...
+        }
+    }
+}
