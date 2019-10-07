cb001ed6e23ec00d25eef1639d9c7ebbe4ba5192
shyboylpf <lpf.mart@gmail.com>
2019-10-07 13:13:18 +0800

    系统自带的CancellationTokenSource

---
+++ ThreadTest63/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cancelSource = new CancellationTokenSource();
+            new Thread(() => Work(cancelSource.Token)).Start();
+            Thread.Sleep(5000);
+            cancelSource.Cancel();
+        }
+
+        private static void Work(CancellationToken cancelToken)
+        {
+            while (true)
+            {
+                cancelToken.ThrowIfCancellationRequested();
+            }
+        }
+    }
+}
