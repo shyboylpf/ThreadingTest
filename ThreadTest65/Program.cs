5e61021db6fff53985279376d34d1fb54e03ecb4
shyboylpf <lpf.mart@gmail.com>
2019-10-07 23:10:54 +0800

    Lazy<T> 懒加载

---
+++ ThreadTest65/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTest65
+{
+    class Program
+    {
+        Lazy<Expensive> _expensive = new Lazy<Expensive>(()=>new Expensive(), true);
+        public Expensive Expensive
+        {
+            get
+            {
+                return _expensive.Value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var exp = new Program().Expensive;
+        }
+    }
+    class Expensive
+    {
+
+    }
+}
