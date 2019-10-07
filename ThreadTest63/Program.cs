504ee4c05f1afcf164a64a150b019e6f11d5cc81
shyboylpf <lpf.mart@gmail.com>
2019-10-07 22:13:30 +0800

    系统自带的CancellationTokenSource

--- ThreadTest63/Program.cs
+++ ThreadTest63/Program.cs
@@ -24,5 +24,7 @@ namespace ThreadTest63
                 cancelToken.ThrowIfCancellationRequested();
             }
         }
+        // The CancellationToken struct provides two additional useful members. The first is WaitHandle, which returns a wait handle that’s signaled when the token is canceled. The second is Register, which lets you register a callback delegate that will be fired upon cancellation.
+        // Most of these classes’ use of cancellation tokens is in their Wait methods. For example, if you Wait on a ManualResetEventSlim and specify a cancellation token, another thread can Cancel its wait. This is much tidier and safer than calling Interrupt on the blocked thread.
     }
 }
