b152b8dc4cdf0ad933ac6c284d20743b6b2547fb
shyboylpf <lpf.mart@gmail.com>
2019-10-08 18:41:33 +0800

    ThreadLocal来保持字段在线程间隔离, random这次增加了种子, 来fix 随机出来的数都是一样的

---
+++ ThreadTest69/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest69
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                new Thread(localRand).Start();
+            }
+        }
+
+        static void localRand()
+        {
+            ThreadLocal<Random> localRandom = new ThreadLocal<Random>(() => new Random(Guid.NewGuid().GetHashCode()));
+            Console.WriteLine("threadLocal: " + localRandom.Value.Next());
+        }
+    }
+}
