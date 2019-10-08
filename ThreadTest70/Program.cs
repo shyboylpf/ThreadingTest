81c116904afa82d31c17c07d8911359722b7d909
shyboylpf <lpf.mart@gmail.com>
2019-10-09 00:41:22 +0800

    GetData and SetData 来进行字段的线程隔离

---
+++ ThreadTest70/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class test
+    {
+        // The same LocalDataStoreSlot object can be used across all threads
+        LocalDataStoreSlot _secSlot = Thread.GetNamedDataSlot("securityLevel");
+        //  Alternatively, you can control a slot’s scope yourself with an unnamed slot, obtained by calling Thread.AllocateDataSlot:
+        LocalDataStoreSlot _secSlot2 = Thread.AllocateDataSlot();
+
+        // This property has a separate value on each thread.
+        int SecurityLevel
+        {
+            get
+            {
+                object data = Thread.GetData(_secSlot);
+                return data == null ? 0 : (int)data;        // null == uninitialized
+            }
+            set
+            {
+                Thread.SetData(_secSlot, value);
+            }
+        }
+    }
+}
