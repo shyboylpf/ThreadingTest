bde56d4aa0899d69eeac15d98c3fc2db631755d0
shyboylpf <lpf.mart@gmail.com>
2019-10-08 15:41:12 +0800

    Interlocked.CompareExchange

---
+++ ThreadTest676/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int _lastRread = 0;
+            int index = 10;
+            while(Interlocked.CompareExchange(ref _lastRread, index+1, index) < index)
+            {
+                index--;
+                Console.WriteLine(_lastRread);
+            }
+            Console.WriteLine("result: " + _lastRread);
+        }
+    }
+}
