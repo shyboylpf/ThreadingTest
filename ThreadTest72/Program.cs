677cb7023413763c9fe4b305073fab8103a031a4
shyboylpf <lpf.mart@gmail.com>
2019-10-09 17:51:35 +0800

    Thread多线程异步编程例子

---
+++ ThreadTest72/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("主线程测试开始..");
+            Thread th = new Thread(ThMethod);
+            th.Start();
+            Thread.Sleep(1000);
+            Console.WriteLine("主线程测试结束..");
+            Console.ReadLine();
+        }
+
+        private static void ThMethod()
+        {
+            Console.WriteLine("异步执行开始");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("异步执行" + i.ToString() + "..");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("异步执行完成");
+        }
+    }
+}
