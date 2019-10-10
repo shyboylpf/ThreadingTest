d57bfb0dac6ebcd86112952e10e2310a2a44394b
shyboylpf <lpf.mart@gmail.com>
2019-10-10 10:14:32 +0800

    System.Threading.Timer,

--- ThreadTest75/Program.cs
+++ ThreadTest75/Program.cs
@@ -13,6 +13,18 @@ namespace ThreadTest75
         {
             // First interval = 5000ms; subsequent intervals = 1000ms
             Timer tmr = new Timer(Tick, "tick...", 5000, 1000);
+
+
+            Thread.Sleep(10000);
+            Console.WriteLine("Timer.Change函数动态改变起始时间和间隔时间");
+            // 可以用change函数动态额改变起始时间和间隔时间
+            tmr.Change(1000, 5000);
+
+            Thread.Sleep(10000);
+            Console.WriteLine("间隔时间改为Timeout.Infinite  使函数仅执行一次");
+            // 如果只想调用一次
+            tmr.Change(1000, Timeout.Infinite);
+
             Console.ReadLine();
             tmr.Dispose();  // this both stops the timer and cleans up.
         }
