a746c83bda5e9cb223e66a181e968612d22f9743
shyboylpf <lpf.mart@gmail.com>
2019-10-14 23:55:32 +0800

    Threading.Timer 0延时启动测试 , 用的也是线程池

---
+++ ThreadTest85/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest85
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new Foo();
+            Console.ReadLine();
+        }
+    }
+
+    internal class Foo
+    {
+        private Timer tmr = null;
+
+        public Foo()
+        {
+            tmr = new Timer(test, null, 0, 1000);
+        }
+
+        private void test(object state)
+        {
+            Console.WriteLine($"start from timer.{Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(2000);
+            Console.WriteLine($"end from timer.{Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
