24d7e1b6562f3cbe19c45400a4c17c9de6aa2b09
shyboylpf <lpf.mart@gmail.com>
2019-10-15 18:19:17 +0800

    Two-Way Signaling and Races 基于Monitor.Wait/Pulse 做的双向信号机制

---
+++ ThreadingTest89/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingTest89
+{
+    internal class Program
+    {
+        private static readonly object _locker = new object();
+        private static bool _ready, _go;
+
+        private static void Main(string[] args)
+        {
+            new Thread(SaySomething).Start();
+
+            for (int i = 0; i < 5; i++)
+                lock (_locker)
+                {
+                    while (!_ready) Monitor.Wait(_locker);
+                    _ready = false;
+                    _go = true;
+                    Monitor.PulseAll(_locker);
+                }
+        }
+
+        private static void SaySomething()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                lock (_locker)
+                {
+                    _ready = true;
+                    Monitor.PulseAll(_locker);
+                    while (!_go) Monitor.Wait(_locker);
+                    _go = false;
+                    Console.WriteLine("Wassup?");
+                }
+            }
+        }
+
+        //public int[] TwoSum(int[] nums, int target)
+        //{
+        //    Dictionary<int, int> map = new Dictionary<int, int>();
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        int complement = target - nums[i];
+        //        if (map.ContainsKey(complement))
+        //        {
+        //            return new int[] { map[complement], i };
+        //        }
+        //        map[nums[i]] = i;
+        //    }
+        //    throw new Exception();
+        //}
+    }
+}
