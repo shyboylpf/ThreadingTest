990457255c61ebe455a0ae27c2a890e39148b7f9
shyboylpf <lpf.mart@gmail.com>
2019-10-15 22:40:59 +0800

    用Monitor.Wait/PulseAll 来实现CountdownEvent

---
+++ ThreadTest91/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTest91
+{
+    /// <summary>
+    /// 用Monitor.Wait/PulseAll 来实现CountdownEvent
+    /// </summary>
+    public class Countdown
+    {
+        private object _locker = new object();
+        private int _value;
+
+        private static void Main(string[] args)
+        {
+        }
+
+        public Countdown()
+        {
+        }
+
+        public Countdown(int initalCount)
+        {
+            _value = initalCount;
+        }
+
+        public void Signal()
+        {
+            AddCount(-1);
+        }
+
+        public void AddCount(int amount)
+        {
+            lock (_locker)
+            {
+                _value += amount;
+                if (_value <= 0)
+                {
+                    Monitor.PulseAll(_locker);
+                }
+            }
+        }
+
+        public void Wait()
+        {
+            lock (_locker)
+            {
+                while (_value > 0)
+                {
+                    Monitor.Wait(_locker);
+                }
+            }
+        }
+    }
+}
