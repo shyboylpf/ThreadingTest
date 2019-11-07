c7434b7c9574d4285f9277eecee23740d96475aa
shyboylpf <lpf.mart@gmail.com>
2019-11-07 18:08:33 +0800

    petapoco初始化

---
+++ petapocoTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HS.DAL;
+using PetaPoco;
+
+namespace petapocoTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //DefaultConnectionDB.Factory = new entryString();
+            var rows = AP_Equ_MattressRunStateChangeList.Single(2);
+            Console.WriteLine(rows.changeDeviceRunState);
+        }
+    }
+
+    public class entryString : DefaultConnectionDB.IFactory
+    {
+        public DefaultConnectionDB GetInstance()
+        {
+            DefaultConnectionDB db = new DefaultConnectionDB("server=123.57.0.0;database=Develop;uid=sa;pwd=encryptstring", "System.Data.SqlClient");
+            return db;
+        }
+    }
+}
