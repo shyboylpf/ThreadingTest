560340f0006f600ed8799ddaf350bfd1cde2ac66
shyboylpf <lpf.mart@gmail.com>
2019-11-18 10:44:39 +0800

    httpwebRequest 不知道为啥 , 总是成功一次 , 失败一次

--- httpRequestTest/Program.cs
+++ httpRequestTest/Program.cs
@@ -22,24 +22,31 @@ namespace httpRequestTest
 
         private static void worker(object sender, ElapsedEventArgs e)
         {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://115.28.182.39:8999/api/QueryCurrentInfusionList");
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-            //httpWebRequest.Headers.Add("UserAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36");
-            string WardNo = "2901";
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            try
             {
-                string json = "{\"Ward\": \"" + WardNo + "\"}";
+                var httpWebRequest = WebRequest.Create("http://115.28.182.39:8999/api/QueryCurrentInfusionList");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                //httpWebRequest.Headers.Add("UserAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36");
+                string WardNo = "2913";
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{\"Ward\": \"" + WardNo + "\"}";
 
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+                var httpResponse = httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    string responseText = streamReader.ReadToEnd();
+                    Console.WriteLine(responseText);
+                }
             }
-            HttpWebResponse httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            catch (Exception ex)
             {
-                string responseText = streamReader.ReadToEnd();
-                Console.WriteLine(responseText);
+                Console.WriteLine(ex.Message);
             }
         }
     }
