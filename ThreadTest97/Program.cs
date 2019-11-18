a73a9dfb33db8d739f11659890279db685635b90
shyboylpf <lpf.mart@gmail.com>
2019-11-18 11:16:45 +0800

    Thread.Abort

---
+++ ThreadTest97/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadTest97
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Thread t = new Thread(delegate () { while (true) ; });
+
+            Console.WriteLine(t.ThreadState); // Unstarted
+
+            t.Start();
+            Thread.Sleep(1000);
+            Console.WriteLine(t.ThreadState);  // running
+
+            t.Abort();
+            Console.WriteLine(t.ThreadState);   // aborted
+
+            t.Join();
+            Console.WriteLine(t.ThreadState);   // aborted
+        }
+    }
+}
