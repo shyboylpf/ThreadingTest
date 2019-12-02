cfefdc16bbccbbe86aa70b07b29d4e51e71afb68
shyboylpf <lpf.mart@gmail.com>
2019-12-02 17:59:06 +0800

    parallel优化 , 输出端优化 , ForAll

---
+++ ThreadTest107/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTest107
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            "abcdef".AsParallel().Select(c => char.ToUpper(c)).ForAll(Console.WriteLine);
+        }
+    }
+}
