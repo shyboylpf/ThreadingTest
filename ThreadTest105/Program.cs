2cf51da5c094e8ce402b61223a2c61a2d711a631
shyboylpf <lpf.mart@gmail.com>
2019-12-02 17:26:43 +0800

    如果需要调整WithDegreeOfParallelism 并行度 , 需要再次调用 asParallel来强制merge和partition

---
+++ ThreadTest105/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTest105
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Changing the degree of parallelism
+        /// private You can private call WithDegreeOfParallelism private only once private within a PLINQ query.If you need to call it again, you must force merging and repartitioning of the query by calling AsParallel() again within the query:
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            "The Quick Brown Fox"
+                .AsParallel().WithDegreeOfParallelism(2)
+                .Where(c => !char.IsWhiteSpace(c))
+                .AsParallel().WithDegreeOfParallelism(3)
+                .Select(c => char.ToUpper(c));
+        }
+    }
+}
