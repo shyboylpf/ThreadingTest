3da784473c4c05e672d0dff04f4684c92f607e3d
shyboylpf <lpf.mart@gmail.com>
2019-12-02 16:12:11 +0800

    parallel  WithDegreeOfParallelism ping 通过并行io耗时操作, 以提高程序执行效率, 这里用了监测网络的ping  ; WithDegreeOfParallelism 确定并发线程数 , 因为不是cpu密集型, 所以多并发一些.

---
+++ ThreadTest103/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTest103
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var items = (from site in new[]
+              {
+              "www.baidu.com",
+              "t.cn",
+              "t.tt",
+            }
+              .AsParallel().WithDegreeOfParallelism(3)
+                         let p = new Ping().Send(site)
+                         select new
+                         {
+                             site,
+                             Result = p.Status,
+                             Time = p.RoundtripTime
+                         });
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.site}|{item.Result}|{item.Time}");
+            }
+        }
+    }
+}
