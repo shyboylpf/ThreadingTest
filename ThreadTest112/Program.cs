3a12f2c4bb70e4146e4430fa92dce28d40ad0482
shyboylpf <lpf.mart@gmail.com>
2019-12-19 14:50:24 +0800

    Parallel.For Parallel.Foreach

---
+++ ThreadTest112/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace ThreadTest112
+{
+    internal class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static void Main(string[] args)
+        {
+            log.Info("test");
+            Parallel.For(1, 10, i => Foo(i));
+            Console.WriteLine("====");
+            Parallel.For(1, 10, Foo);
+
+            Parallel.ForEach("Hello , World", Foo);
+
+            var keyPairs = new string[6];
+            Parallel.For(0, keyPairs.Length, i => keyPairs[i] = RSA.Create().ToXmlString(true));
+            Parallel.ForEach(keyPairs, i => Console.WriteLine(i));
+
+            string[] keyPairs2 =
+                ParallelEnumerable.Range(0, 6)
+                .Select(i => RSA.Create().ToXmlString(true))
+                .ToArray();
+            Parallel.ForEach(keyPairs2, i => Console.WriteLine(i));
+        }
+
+        private static void Foo(int i)
+        {
+            log.Info(i);
+        }
+
+        private static void Foo(char c)
+        {
+            log.Info(c);
+        }
+    }
+}
