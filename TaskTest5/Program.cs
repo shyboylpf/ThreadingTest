0dbff9c5e40107817c34f275992e9af8ad4b1dad
朋朋 <lpf.mart@gmail.com>
2020-06-03 00:44:20 +0800

    计算密集型操作Task

--- TaskTest5/Program.cs
+++ TaskTest5/Program.cs
@@ -14,6 +14,23 @@ namespace TaskTest5
         private static void Main(string[] args)
         {
         }
+
+        internal Task<Bitmap> RenderAsync(ImageData data, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                var bmp = new Bitmap(10, 10);
+                for (int y = 0; y < 10; y++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    for (int x = 0; x < 10; x++)
+                    {
+                        // render pixel [x,y] into bmp
+                    }
+                }
+                return bmp;
+            }, cancellationToken);
+        }
     }
 
     //internal Task<Bitmap> RenderAsync(ImageData data, CancellationToken cancellationToken)
@@ -32,21 +49,4 @@ namespace TaskTest5
     //        return bmp;
     //    }, cancellationToken);
     //}
-
-    internal Task<Bitmap> RenderAsync(ImageData data, CancellationToken cancellationToken)
-    {
-        return Task.Run(() =>
-        {
-            var bmp = new Bitmap(10, 10);
-            for (int y = 0; y < 10; y++)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-                for (int x = 0; x < 10; x++)
-                {
-                    // render pixel [x,y] into bmp
-                }
-            }
-            return bmp;
-        }, cancellationToken);
-    }
 }
