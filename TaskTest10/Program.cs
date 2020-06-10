367b1cd004ea09bd5b9f9be01b23dc91fa0addf6
朋朋 <lpf.mart@gmail.com>
2020-06-10 14:04:58 +0800

    Task.Yield（） This is equivalent to asynchronously posting or scheduling back to the current context. 这相当于用异步的方式调度会当前上下文

---
+++ TaskTest10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Task.Run(async delegate
+            {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    await Task.Yield(); // fork the continuation into a separate work item.
+                }
+            });
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            //Task.Delay(TimeSpan.FromSeconds(1000));
+        }
+    }
+}
