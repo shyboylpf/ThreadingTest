9dde66e1c61732d3436d54263ad40d3ea9fac584
朋朋 <lpf.mart@gmail.com>
2020-06-10 14:49:29 +0800

    Task.ConfigureAwait(), use the Task.ConfigureAwait method to inform the await operation not to capture and resume on the context, but to continue execution wherever the asynchronous operation that was being awaited completed:使用Task.ConfigureAwait方法，指示等待操作不要捕获和恢复上下文，而是继续执行正在等待完成的所有异步操作

---
+++ TaskTest11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest11
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            Task.Run(async () => await task().ConfigureAwait(continueOnCapturedContext: false));
+            Console.WriteLine("End");
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static Task task()
+        {
+            return Task.Run(delegate
+            {
+                Console.WriteLine("1");
+            });
+        }
+    }
+}
