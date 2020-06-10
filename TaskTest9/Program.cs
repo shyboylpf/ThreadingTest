6e409ee61a52be108bef061aa7f19129936ddb8d
朋朋 <lpf.mart@gmail.com>
2020-06-10 13:55:53 +0800

    await a Task

--- TaskTest9/Program.cs
+++ TaskTest9/Program.cs
@@ -17,7 +17,15 @@ namespace TaskTest9
          * Under the covers, the await functionality installs a callback on the task by using a continuation. This callback resumes the asynchronous method at the point of suspension. When the asynchronous method is resumed, if the awaited operation completed successfully and was a Task<TResult>, its TResult is returned.
          * await使用continuation来注册了一个回调，此回调在挂起时恢复异步方法，如果等待的方法执行结束，则返回TResult
          * Typically, this is the default task scheduler (TaskScheduler.Default), which targets the thread pool.
-         *
+         * When an asynchronous method is called, it synchronously executes the body of the function up until the first await expression on an awaitable instance
+         * that has not yet completed, at which point the invocation returns to the caller. If the asynchronous method does not return void, a Task or Task<TResult>
+         * object is returned to represent the ongoing computation. In a non-void asynchronous method, if a return statement is encountered or the end of the method
+         * body is reached, the task is completed in the RanToCompletion final state. If an unhandled exception causes control to leave the body of the asynchronous
+         * method, the task ends in the Faulted state. If that exception is an OperationCanceledException, the task instead ends in the Canceled state. In this
+         * manner, the result or exception is eventually published.
+         * There are several important variations of this behavior. For performance reasons, if a task has already completed by the time the task is awaited,
+         * control is not yielded, and the function continues to execute. Additionally, returning to the original context isn't always the desired behavior
+         * and can be changed; this is described in more detail in the next section.
          */
 
         private static void Main(string[] args)
