fc327e197b9e0dfd3f03213b7bed249999fce35a
朋朋 <lpf.mart@gmail.com>
2020-06-10 15:44:37 +0800

    Progress<int> progress 监视进度

---
+++ TaskTest13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTest13
+{
+    internal class Program
+    {
+        private object pbDwonloadProgress;
+
+        private static void Main(string[] args)
+        {
+        }
+
+        // 监视进度
+        // 某些异步方法通过传入异步方法的进度接口来公开进度
+        // WPF里会用
+        private async void btnDownload_Click(object sender, RouteEventArgs e)
+        {
+            btnDownload.IsEnabled = false;
+            try
+            {
+                txtResult.Text = await DownloadStringAsync(TxtUrl.Text,
+                    new Progress<int>(p => pbDwonloadProgress.Value = p));
+            }
+            finally
+            {
+                btnDownload.IsEnabled = true;
+            }
+        }
+
+        private Task DownloadStringAsync(object text, Progress<int> progress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class RouteEventArgs
+    {
+    }
+}
